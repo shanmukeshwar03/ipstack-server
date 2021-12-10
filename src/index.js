d524632c19a1116b0fd37d2d63091d0a1f4c6d89
shanmukeshwar03 <shanmukeshwar03@gmail.com>
2021-12-11 00:46:32 +0530

    minor updates

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import cors from 'cors'
 import fetch from 'node-fetch'
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded())
 app.use(cors())
+
 app.get('/', async (req, res) => {
   try {
-    const clientIP =
+    let clientIP =
       req.headers['x-forwarded-for'] || req.connection.remoteAddress
     if (clientIP.includes(',')) clientIP = clientIP.split(',')[0]
-    console.log(clientIP)
     const response = await fetch('http://ip-api.com/json/' + clientIP)
     const decode = await response.json()
     res.send(decode)
