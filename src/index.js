e90a2a6c69d98a789394904a6e1f5b848c64fa8a
shanmukeshwar03 <shanmukeshwar03@gmail.com>
2021-12-11 10:59:17 +0530

    switch to axios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import cors from 'cors'
-import fetch from 'node-fetch'
+import axios from 'axios'
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 app.get('/', async (req, res) => {
@@ -12,9 +12,8 @@ app.get('/', async (req, res) => {
     let clientIP =
       req.headers['x-forwarded-for'] || req.connection.remoteAddress
     if (clientIP.includes(',')) clientIP = clientIP.split(',')[0]
-    const response = await fetch('http://ip-api.com/json/' + clientIP)
-    const decode = await response.json()
-    res.send(decode)
+    const response = await axios.get('http://ip-api.com/json/' + clientIP)
+    res.send(response.data)
   } catch (error) {
     res.status(500).send('Unable to fetch IP')
   }
