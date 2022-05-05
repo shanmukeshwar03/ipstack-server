90ae69bc49308d5b9e7bdec401aa6a23de919adc
shanmukeshwar03 <shanmukeshwar03@gmail.com>
2022-05-05 20:17:25 +0530

    Minor updates

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #/bin/bash
 
-echo 'Deploy started...'
-rsync -av --exclude='node_modules' --exclude='.git' ../nodejs-ipstack/ ubuntu@152.70.74.152:~/projects/ipstack
-ssh ubuntu@152.70.74.152 "cd ~/projects/ipstack && docker-compose up -d --build"
-echo 'Deployed!'
+docker context use cluster
+docker-compose up -d --build
